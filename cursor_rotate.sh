52c25b1619fb6ad29d0986f8c7fbfd8c9f9a8cfa
Nandhakumar Madheshwaran <63140143+nandha-coder@users.noreply.github.com>
2020-04-04 16:40:39 +0530

    Update cursor_rotate.sh
    Updated

--- cursor_rotate.sh
+++ cursor_rotate.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-#Script to show rotating cursur for the time being 
+#Script to show rotating cursur for the time being
 #Time should be mentioned as a arguement
-#syntax			./rotate.sh time(Seconds)
+#syntax                 ./rotate.sh time(Seconds)
 MAGENTA="\e[35m"
 BOLD="\e[1m"
 NOCOLOR="\e[39m"
 tme=$1
 spin='\|/-\'
 
+#Checking the Input Argument count
+#If it is equal to 1
+if [ $# -eq 1  ]
+then
 printf "$BOLD $MAGENTA"
-for (( start = $1; start > 0; start-- )) 
+for (( start = $1; start > 0; start-- ))
+do
+{
+	for i in {1..4}
 	do
-	{
-			for i in {1..4}
-				do
-					sleep 0.5 &
-					printf "\r ${spin:$i:1} ${spin:$i:1} ${spin:$i:1}" & sleep 0.5
-				done
-		#printf " $start \r"
-		let "tme=tme-1"
-	}
+	sleep 0.5 &
+	printf "\r ${spin:$i:1} ${spin:$i:1} ${spin:$i:1}" & sleep 0.5
 	done
+let "tme=tme-1"
+}
+done
 printf "\r$NOCOLOR"
+
+#Input Argument is not equal to 1
+else
+echo "Wrong Syntax Usage : cursor_rotate.sh count[ in seconds]"
+fi
