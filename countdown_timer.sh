4ed626589cf0ce5424508e6d860bcfcfce992200
Nandhakumar Madheshwaran <63140143+nandha-coder@users.noreply.github.com>
2020-04-04 16:54:27 +0530

    Update and rename timer.sh to countdown_timer.sh
    Comments updated

--- countdown_timer.sh
+++ countdown_timer.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+## Count down timer
+## Usage : ./countdown_timer.sh
+## DATE: 4th April 2020
+## Author: Nandhakumar Madheshwaran
+
 printf "\n Please provide the time :"
 read tme
 for (( i=$tme; i>0; i--))
