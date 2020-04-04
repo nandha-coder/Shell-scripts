9c0149dfef6622cd4d0c0f516029e00771463259
Nandhakumar Madheshwaran <63140143+nandha-coder@users.noreply.github.com>
2020-04-04 15:01:32 +0530

    Update Linux_Utilisation_with_Email_Trigger.sh
    Added spaces

--- Linux_Utilisation_with_Email_Trigger.sh
+++ Linux_Utilisation_with_Email_Trigger.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 ## CPU Utilisation & Memory Utilisation
-## Catagarise the alert level as "CRITICAL" / "WARNING" / "OK"
-## 
-## In case of other than "OK" level will trigger automatic Email
-## Email Address needs to be noted in Variable declaration
 ## DATE: 4th April 2020
 ## Author: Nandhakumar Madheshwaran
-
-
 #COLOR CODES
 NOCOLOR="\e[39m"
 RED="\e[31m"
@@ -16,7 +10,7 @@ GREEN="\e[32m"
 
 #VARIABLE DECLARATIONS
 SERVER_HOSTNAME=`uname -n`
-ADMIN_EMAIL_ID="root@localhost"
+ADMIN_EMAIL_ID=root@localhost
 DATE=$(date "+%F %H:%M:%S")
 CPUUSAGE=$(sar -u 3 3 | grep -i Average | awk '{print $NF}')
 MEMUSAGE=$(sar -r 3 3 | grep Average: | awk '{print $4}')
@@ -24,19 +18,23 @@ MEMUSAGE=$(sar -r 3 3 | grep Average: | awk '{print $4}')
 #To convert decimal point Value to integer
 SAR_CPU=${CPUUSAGE/.*}
 SAR_MEM=${MEMUSAGE/.*}
+
 ##########  CPU conditions ########
 if [ $SAR_CPU -le 10 ]
 then
 printf "${RED}CRITICAL ${NOCOLOR} CPU Usage : ${SAR_CPU}%% Idle ${DATE}\n" >> /opt/cpu.out
 tail -n5 /opt/cpu.out > /tmp/cpuusage.tmp
 echo Sending Email....
-mail -s "CPU Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/cpusage.tmp
+#mail -s "CPU Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/cpusage.tmp
+
+
 elif [ $SAR_CPU -ge 10 ] && [ $SAR_CPU -le 20 ]
 then
 printf "${YELLOW}WARNING ${NOCOLOR} CPU Usage : ${SAR_CPU}%% Idle ${DATE}\n" >> /opt/cpu.out
 tail -n5 /opt/cpu.out > /tmp/cpuusage.tmp
 echo Sending Email....
-mail -s "CPU Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/cpusage.tmp
+#mail -s "CPU Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/cpusage.tmp
+
 else
 printf "${GREEN}OK ${NOCOLOR} CPU Usage : ${SAR_CPU}%% Idle ${DATE}\n" >> /opt/cpu.out
 fi
@@ -47,14 +45,14 @@ then
 printf "${RED}CRITICAL ${NOCOLOR} Mem Usage : ${SAR_MEM}%% Used ${DATE}\n" >> /opt/mem.out
 tail -n5 /opt/mem.out > /tmp/memusage.tmp
 echo Sending Email....
-mail -s "Mem Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/memusage.tmp
+#mail -s "Mem Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/memusage.tmp
 
 elif [ $SAR_MEM -ge 80 ] && [ $SAR_MEM -le 90 ]
 then
 printf "${YELLOW}WARNING ${NOCOLOR} Mem Usage : ${SAR_CPU}%% Used ${DATE}\n" >> /opt/mem.out
 tail -n5 /opt/mem.out > /tmp/memusage.tmp
 echo Sending Email....
-mail -s "Mem Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/memusage.tmp
+#mail -s "Mem Utilization of ${SERVER_HOSTNAME}" "$ADMIN_EMAIL_ID" < /tmp/memusage.tmp
 else
 printf "${GREEN}OK ${NOCOLOR} Mem Usage : ${SAR_MEM}%% Used ${DATE}\n" >> /opt/mem.out
 fi
